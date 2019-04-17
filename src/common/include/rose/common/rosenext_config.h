578711588b6f884bfa1ac491750fb4eba59366dc
Ralph Minderhoud <ralphminderhoud@gmail.com>
2019-04-17 22:41:02 +0500

    Set max stat to 250

--- src/common/include/rose/common/rosenext_config.h
+++ src/common/include/rose/common/rosenext_config.h
@@ -2,10 +2,11 @@
 #define ROSENEXT_CONFIG_H
 #pragma once
 
+
 namespace Rose {
 namespace Config {
 	const uint8_t MAX_LEVEL = 130;
-	const uint16_t MAX_STAT = 300;
+	const uint16_t MAX_STAT = 250;
 }
 }
 
