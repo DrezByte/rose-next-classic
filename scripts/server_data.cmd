445317007230c5082860aa3d3e5e194c59a92cb4
Ralph Minderhoud <ralphminderhoud@gmail.com>
2019-12-01 22:49:14 +0500

    Improve server data script

--- scripts/server_data.cmd
+++ scripts/server_data.cmd
@@ -1,2 +1,12 @@
-mklink /J %~dp0\..\server\data\3ddata %~dp0..\game\3ddata
+@echo off
+set SERVER_DATA=%~dp0\..\server\data\3ddata
+
+IF NOT EXIST %SERVER_DATA% (
+	mkdir %SERVER_DATA%\..
+	mklink /J %SERVER_DATA% %~dp0..\game\3ddata
+) ELSE (
+	echo %SERVER_DATA% already exists
+)
+
+
 pause
