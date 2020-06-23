44ac843d69deeb29d7722bda8fc7d0b3de1ec3d8
Ralph Minderhoud <ralphminderhoud@gmail.com>
2020-06-23 09:08:42 -0500

    Fix bug in worldserver logout check
    The check incorrectly returned a warning when the logout was completed
    correctly.

--- src/sho_worldserver/src/cws_account.cpp
+++ src/sho_worldserver/src/cws_account.cpp
@@ -6,7 +6,7 @@
 #include "WS_SocketLSV.h"
 
 CWS_Account::~CWS_Account() {
-    if (0 == m_btLoginBIT) {
+    if (0 != m_btLoginBIT) {
         const char* szAccount = this->m_Account.Get() ? this->m_Account.Get() : "unknown user";
         LOG_WARN("Invalid logout process found for {}", szAccount);
     }
