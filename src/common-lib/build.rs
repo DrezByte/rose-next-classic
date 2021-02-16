45ce5e51258276942c30e4684910c88e06d2b9df
Ralph Minderhoud <ralphminderhoud@gmail.com>
2021-02-16 12:39:17 -0600

    Refactor project flatbuffer integration
    Refactored the usage of flatbuffers in the project to be more
    consistent. Now, the common-lib project will build the packets in its
    build script and the common vs project will build common-lib to trigger
    building of the packets. Additionally moved the gen dirs and add rust
    outputs for flatbuffers.
    
    - Moved rose packets to src/common-lib/packets
    - Removed vs integration for flatbuffer compiling
    - Added flatbuffer compiling to common-lib build script
    - Added rust target for flatbuffer compiling
    - Changed gen output dir from build/{config}/gen to build/gen since it's
      the same regardless of build config
    - Excluded launcher from workspace build since it is still WIP

--- src/common-lib/build.rs
+++ src/common-lib/build.rs
@@ -1,4 +1,9 @@
+use std::env;
+use std::path::{Path, PathBuf};
+
 use cbindgen;
+use flatc_rust;
+
 
 fn main() {
   let crate_dir = env!("CARGO_MANIFEST_DIR");
@@ -7,4 +12,74 @@ fn main() {
   cbindgen::generate_with_config(&crate_dir, config)
     .expect("Failed to generate bindings")
     .write_to_file("include/rose/common/common_interface.h");
+
+  let config = if env::var("DEBUG").is_ok() {
+    "debug"
+  } else {
+    "release"
+  };
+
+  let crate_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+  let root_dir = &crate_dir.parent().unwrap().parent().unwrap();
+  let packet_dir = crate_dir.join("packets");
+
+  let mut out_dir_cpp = PathBuf::from(&root_dir);
+  out_dir_cpp.push("build");
+  out_dir_cpp.push("gen");
+  out_dir_cpp.push("common");
+  out_dir_cpp.push("rose");
+  out_dir_cpp.push("network");
+  out_dir_cpp.push("packets");
+
+  let out_dir_rs = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+  let mut flatc_path = PathBuf::from(&root_dir);
+  flatc_path.push("bin");
+  flatc_path.push(config);
+  flatc_path.push("thirdparty");
+  flatc_path.push("flatc.exe");
+
+  let flatbuffers = &[
+    "common/char_move_mode.fbs",
+    "common/vec3.fbs",
+    "char_create_req.fbs",
+    "char_move_attack.fbs",
+    "char_move.fbs",
+    "login_rep.fbs",
+    "login_req.fbs",
+    "packet_data.fbs",
+    "update_stats.fbs",
+  ];
+
+  eprintln!("{}", flatc_path.display());
+
+  let input_paths: Vec<PathBuf> = flatbuffers.iter().map(|p|
+    packet_dir.join(p)
+  ).collect();
+  let input: Vec<&Path> = input_paths.iter().map(|p| p.as_path()).collect();
+
+  let flatc = flatc_rust::Flatc::from_path(&flatc_path);
+  flatc.check().expect(&format!("{} is not valid", &flatc_path.display()));
+
+  let args_rs = flatc_rust::Args {
+    lang: "rust",
+    inputs: input.as_slice(),
+    out_dir: &out_dir_rs,
+    ..Default::default()
+  };
+
+  let args_cpp = flatc_rust::Args {
+    lang: "cpp",
+    inputs: &input,
+    out_dir: &out_dir_cpp,
+    extra: &["--scoped-enums"],
+    ..Default::default()
+  };
+
+  for path in &input {
+    println!("cargo:rerun-if-changed={}", path.display());
+  }
+
+  flatc.run(args_rs).expect("flatc: Failed to generate rust code");
+  flatc.run(args_cpp).expect("flatc: Failed to generate cpp code");
 }
